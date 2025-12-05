6dd4f8bba44bd115a2d60190cb151123f7aceeb7
Abdu Radi <92768651+AbduKhRadi@users.noreply.github.com>
2025-12-05 17:39:42 -0500

    Initial commit

---
+++ dancesage/dancesage/dancesageApp.swift
@@ -0,0 +1,17 @@
+//
+//  dancesageApp.swift
+//  dancesage
+//
+//  Created by Abdu Radi on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct dancesageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
